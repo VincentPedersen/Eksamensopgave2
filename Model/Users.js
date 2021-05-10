637c5141d9fbeb2dc355193942d5dc548c3a240d
Vincent Pedersen <vipe@navipartner.dk>
2021-05-11 01:23:21 +0200

    Ok guess that wasn't the last commit

--- Model/Users.js
+++ Model/Users.js
@@ -1,6 +1,3 @@
-const bcrypt = require('bcrypt')
-const saltRounds = 10;
-
 
 class User {
     constructor (id,email,password,first_name,last_name,age,location,gender,interests1,interests2,interests3,prefferedSex1,prefferedSex2,prefferedSex3){
@@ -18,35 +15,17 @@ class User {
         this. prefferedSex1 = prefferedSex1;
         this. prefferedSex2 = prefferedSex2;
         this. prefferedSex3 = prefferedSex3;
-        //this.match = match;
-        //this.image = image; 
     }
 }
 
-class PaymentUser extends User {
-    constructor (username,password,gender,nationality,location,prefferedSex,CreditCard,interests){
-    super(username,password,gender,nationality,location,prefferedSex,interests);
-    this.CreditCard = CreditCard; 
-    }
-}
-
-class FreeUser extends User {
-    constructor (username,password,gender,nationality,location,prefferedSex,interests){
-        super(username,password,gender,nationality,location,prefferedSex,interests);
-    }
-
-    
-
-}
 
 
    
-//Conjoins the two users in an array
-var Array = [PaymentUser,FreeUser];
 
 
 
-//Allows the array to be found in the other files 
+
+//Allows the user to be found in other files 
 
 module.exports = User; 
  
